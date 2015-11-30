3b52d49fc0311749c28246f4b849d1576b2e3225
Roni <ronikrakover@gmail.com>
2015-11-30 09:31:05 +0200

    nov 30 morning

--- public/js/games.js
+++ public/js/games.js
@@ -1,7 +1,24 @@
 // TODO: use ajax to get the file server/games.json
 // Create the list
 
-$.getJSON('api/game', function (games) {
+$.getJSON('api/games', function (games) {
     console.log(games);
+    var elGames = document.querySelector("#listGames");
+    elGames.innerHTML = '';
+    var str = '';
+    $.each(games, function (key, value) {
+        str +=
+            '<div class="gameContainer">' +
+                '<div class="gameName">'+value.name+
+                '</div>'+
+            '<div class ="gameImage"><img src="' + value.picture +
+            '" title="'+ value.description+'">' +
+            '</div>' +
+                '<div class="gameInfo">'+value.description+
+            '</div></div>'
+    })
+
+    elGames.innerHTML = str;
+
 })
 
